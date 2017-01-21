e0750cc6e7062415b5c12338d5a3ae0becfa44ba
Raphael Chang <raphaelchang1227@gmail.com>
2017-01-20 18:44:58 -0800

    setup mongodb

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-/* Hello, World! program in node.js */
-console.log("Hello World!");
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/carouseldb';
+
+
+
+
+
